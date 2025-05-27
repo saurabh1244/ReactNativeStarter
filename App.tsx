9cb90f5840efc855591718df7b74221d1eccceb5
Saurabh Chandra <saurabhchandra1244@gmail.com>
2025-05-27 13:32:28 +0530

    zustand added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
-import { TamaguiProvider, Button, YStack } from 'tamagui'
+import React, { useState } from 'react'
+import { TamaguiProvider, Input, Button, Text, YStack } from 'tamagui'
 import config from './tamagui.config'
 
+import { useUserStore } from './src/store/useUserStore'
+
 export default function App() {
+  const [inputValue, setInputValue] = useState('')
+  const count = useUserStore((state) => state.count)
+  const increment = useUserStore((state) => state.increment)
+  const decrement = useUserStore((state)=>state.decrement)
+
   return (
     <TamaguiProvider config={config}>
-      <YStack flex={1} justifyContent="center" alignItems="center" bg="red">
-        <Button theme="dark" color="$white">
-          Hello from Tamagui
+      <YStack f={1} jc="center" ai="center" gap="$4">
+
+        <Text fontSize="$8" color="white">
+          {count}
+        </Text>
+       
+        <Button onPress={increment}>
+          Increment  XX
         </Button>
+
+         <Button onPress={decrement}>
+          Decement
+        </Button>
+
+      
       </YStack>
     </TamaguiProvider>
   )
